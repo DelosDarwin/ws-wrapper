30620cfe4e3bb9fc5bb2bf7fd9dce6660c9bf559
den <den.logunov@gmail.com>
2018-10-30 19:56:03 +0200

    Add reconnectDelay, add prev events handlers saving, update README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ws
 
+Wrapper around "ws" npm package with implementation of websocket reconnection when connection's readyState changes to CLOSED ('close' event is fired).
+
+## Usage
+
+```javascript
+const WebSocketWrapper = require('ws-wrapper');
+
+const ws = new WebSocketWrapper({
+    url: 'http://localhost:3030',
+    reconnectDelay: 3000    // optional, default is 1000
+}).ws;
+
+ws.on('open', e => { ... });
+ws.on('message', e => { ... });
+ws.on('close', e => { ... });
+ws.on('error', e => { ... });
+```
+
+We need "ws" property from WebSocketWrapper because WebSocketWrapper isn't extended from WebSocket ('ws' package), but it has ws property which is a child of WebSocket ('ws' package) class.
--- index.js
+++ index.js
@@ -1,43 +1,45 @@
 const WebSocket = require('ws');
 
 class WebSocketWrapper {
-    constructor({ url, onOpen, onClose, onError }) {
-        this.onOpen = onOpen;
-        this.onClose = onClose;
-        this.onError = onError;
+    constructor({ url, reconnectDelay }) {
+        this.reconnectDelay = reconnectDelay || 1000;
         this.init(url);
     }
 
     init(url) {
-        this.ws = new WebSocket(url);
-        this.url = url;
+        if (this.ws) {
+            const prevEventHandlers = { ...this.ws._events };
 
-        this.ws.on('close', () => {
-            this.onclose();
-            this.onClose && this.onClose();
-        });
+            this.ws = new WebSocket(url);
+            this.ws._events = { ...prevEventHandlers };
+        } else {
+            this.ws = new WebSocket(url);
+            this.url = url;
 
-        this.ws.on('open', () => {
-            console.log('Connection is open');
-            
-            if (this.interval) clearInterval(this.interval);
-            this.onOpen && this.onOpen();
-        });
+            this.ws.on('close', () => {
+                this.onclose();
+            });
 
-        this.ws.on('error', () => {
-            this.onclose();
-            this.onError && this.onError();
-        });
+            this.ws.on('open', () => {
+                console.log('Connection is open');
+                
+                if (this.interval) clearInterval(this.interval);
+            });
+
+            this.ws.on('error', () => {
+                this.onclose();
+            });
+        }
     }
 
     onclose() {
         if (this.interval) clearInterval(this.interval);
-        
+
         this.interval = setInterval(() => {
             console.log('Trying to reconnect to ', this.url);
-            
+
             this.init(this.url);
-        }, 1000); 
+        }, this.reconnectDelay); 
     }
 };
 
